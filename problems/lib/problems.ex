c69e71b3354df91f7b5f2f01de1c696d20a421f9
Dominick Triola <dominick@ignovadesign.com>
2017-03-07 19:26:23 -0800

    refactor

--- problems/lib/problems.ex
+++ problems/lib/problems.ex
@@ -9,17 +9,18 @@ defmodule Problems do
       iex> Problems.fizz_buzz(20)
       [1, 2, "Fizz", 4, "Buzz", "Fizz", 7, 8, "Fizz", "Buzz", 11,
        "Fizz", 13, 14, "FizzBuzz", 16, 17, "Fizz", 19, "Buzz"]
+
   """
   def fizz_buzz(max) do
     for i <- (1..max) do
-      case {15, 5, 3} do
-        {x, 5, 3} when rem(i, x) == 0 ->
+      cond do
+        rem(i, 15) == 0 ->
           "FizzBuzz"
-        {15, x, 3} when rem(i, x) == 0 ->
+        rem(i, 5) == 0 ->
           "Buzz"
-        {15, 5, x} when rem(i, x) == 0 ->
+        rem(i, 3) == 0 ->
           "Fizz"
-        _ ->
+        true ->
           i
       end
     end
@@ -32,6 +33,7 @@ defmodule Problems do
       0
       iex> Problems.sum([1, 2, 3, 4])
       10
+
   """
   def sum([]) do
     0
