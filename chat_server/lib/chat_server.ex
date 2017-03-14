dc088b4bc9bf0fc8af79246922002e321c94f07a
Dominick Triola <dominick@ignovadesign.com>
2017-03-14 08:46:06 -0700

    basic looping chat server

--- chat_server/lib/chat_server.ex
+++ chat_server/lib/chat_server.ex
@@ -1,7 +1,14 @@
 defmodule ChatServer do
   def loop do
+    loop([])
+  end
+  def loop(state) do
     receive do
-      {:get, pid} -> send(pid, [])
+      {:create, msg} ->
+        loop(state ++ [msg])
+      {:get, from} ->
+        send(from, state)
+        loop(state)
     end
   end
 end
