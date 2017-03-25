ad41ee53c4f6ac57e10a83ec84882486f32ec260
Dominick Triola <dominick@ignovadesign.com>
2017-03-25 15:19:42 -0700

    allow for different rooms

--- chat/apps/chat_web/web/channels/room_channel.ex
+++ chat/apps/chat_web/web/channels/room_channel.ex
@@ -1,17 +1,16 @@
 defmodule ChatWeb.RoomChannel do
   use Phoenix.Channel
 
-  def join("room:lobby", _message, socket) do
-    ChatServer.Supervisor.start_room("lobby")
-    chat_list = ChatServer.get("lobby")
+  def join("room:" <> room_name, _message, socket) do
+    ChatServer.Supervisor.start_room(room_name)
+    chat_list = ChatServer.get(room_name)
     {:ok, chat_list, socket}
-  end
-  def join("room:" <> _private_room_id, _params, _socket) do
-    {:error, %{reason: "unauthorized"}}
   end
 
   def handle_in("new_msg", %{"body" => body}, socket) do
-    ChatServer.create("lobby", body)
+    "room:" <> room_name = socket.topic
+    ChatServer.create(room_name, body)
+    ChatServer.get(room_name)
     broadcast! socket, "new_msg", %{body: body}
     {:noreply, socket}
   end
